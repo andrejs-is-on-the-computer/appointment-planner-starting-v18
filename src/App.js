43e8ee6017974511e2b79499e0e611b510992b3e
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-11-14 18:44:29 +0100

    2 - app - state, passing props

--- src/App.js
+++ src/App.js
@@ -9,17 +9,33 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   /*
   Implement functions to add data to
   contacts and appointments
   */
+  const addContact = (newCon) => {
+    setContacts(contacts => [...contacts, newCon]);
+  };
+
+  const addAppointment = (newApt) => {
+    setAppointments(appointments => [...appointments, newApt]);
+  };
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+      <Route path={ROUTES.CONTACTS} element={ <ContactsPage 
+                                                contacts={contacts} 
+                                                addContact={addContact} 
+                                              /> /* Add props to ContactsPage */ }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage 
+                                                    appointments={appointments} 
+                                                    contacts={contacts}
+                                                    addAppointment={addAppointment} 
+                                                  /> /* Add props to AppointmentsPage */ }/>
     </Route>
   ));
   
